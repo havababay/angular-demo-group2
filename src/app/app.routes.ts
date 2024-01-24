8487fa04ba0e6cfa8804047b30ecc173ff1a308b
havababay <hava.babay@gmail.com>
2024-01-24 20:43:10 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { BindingComponent } from './binding/binding.component';
+import { CommunicationComponent } from './communication/communication.component';
+import { NgforDemoComponent } from './ngfor-demo/ngfor-demo.component';
+import { NgifDemoComponent } from './ngif-demo/ngif-demo.component';
+import { HomeDemoComponent } from './home-demo/home-demo.component';
+import { RouterDemoComponent } from './router-demo/router-demo.component';
+import { FormsDemoComponent } from './forms-demo/forms-demo.component';
+import { PersonsListComponent } from './persons-list/persons-list.component';
+import { PersonFormComponent } from './person-form/person-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "binding", component: BindingComponent},
+    {path: "communication", component: CommunicationComponent},
+    {path: "ngfor", component: NgforDemoComponent},
+    {path: "ngif", component: NgifDemoComponent},
+    {path: "", component: HomeDemoComponent},
+    {path: "routerdemo/:id", component: RouterDemoComponent},
+    {path: "forms/:id", component: FormsDemoComponent},
+    {path: "newform", component: FormsDemoComponent},
+    {path: "persons", component: PersonsListComponent},
+    {path: "newperson", component: PersonFormComponent},
+    {path: "person/:idString", component: PersonFormComponent}
+];
